1a1288bedcb40ebb6401b9248d7d0275657e4144
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-01-29 13:48:39 -0600

    Added Code for BK1 Ch. 12 and BK 2 Ch7...still broke

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -12,17 +12,21 @@ JournalEntryComponent()
 // DOM reference to where all entries will be rendered
 const entryLog = document.querySelector(".label")
 
-let useJournalEntries = ""
+
 export const EntryListComponent = () => {
     // Use the journal entry data from the data provider component
     const entries = useJournalEntries()
-
+    let entryHTMLRepresentations = ""
     for (const entry of entries) {
         /*
             Invoke the component that returns an
             HTML representation of a single entry
         */
        entryHTMLRepresentations += JournalEntryComponent(entries)
-        entryLog.innerHTML += ???
+        entryLog.innerHTML += `
+            <article class="entryList"
+                ${entryHTMLRepresentations}
+            </article>
+        `
     }
 }
